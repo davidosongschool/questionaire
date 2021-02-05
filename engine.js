f4f573d5e0311087acfa31170db10d531ccde3f4
unknown <davidosongschool@gmail.com>
2021-02-05 15:48:20 +0000

    top of page js

--- engine.js
+++ engine.js
@@ -89,7 +89,7 @@ $(".contain-quiz").on("click", ".select-options", function () {
 $(".continue-btn").click(function () {
 
     // Scroll to top of page when it is clicked
-    window.scrollTo(0, 0);
+    scroll(0,0);
 
     if (position == 2) {
 
